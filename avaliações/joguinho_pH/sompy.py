5c1a4d9acd1793e236be35f6ae3dede18e6b6203
Lucas Barbosa Barreto <lucasbarbosabarreto551@gmail.com>
2023-11-08 22:25:47 -0300

    dddddddddddd

--- avaliações/joguinho_pH/sompy.py
+++ avaliações/joguinho_pH/sompy.py
@@ -1,2 +1,14 @@
-from playsound import playsound
-playsound("perdeu.mp3")
+import pygame.mixer
+
+# Inicialize o mixer pygame
+pygame.mixer.init()
+
+# Carregue o arquivo MP3
+pygame.mixer.music.load("avaliações/joguinho_pH/ganhou.mp3")
+
+# Reproduza o arquivo MP3
+pygame.mixer.music.play()
+
+# Aguarde o término da música
+while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)
--- avaliações/joguinho_pH/testSom.c
+++ avaliações/joguinho_pH/testSom.c
@@ -1,14 +1,32 @@
-#include <stdio.h>
-#include <Windows.h>
+#include <windows.h>
 #include <mmsystem.h>
-#define PlaySound PlaySoundA
 
-int main(int argc, char *argv){
-    PlaySound("ganhou.wav", NULL, SND_SYNC | SND_FILENAME );
-    Sleep(2000);
-    system("pause");
-    PlaySound(NULL,0,0);
-    system("pause");
+#pragma comment(lib, "winmm.lib")
 
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Uso: %s avaliações/joguinho_pH/ganhou.mp3\n", argv[0]);
+        return -1;
+    }
 
+    // Abre o arquivo MP3
+    if (mciSendStringA("open \"" argv[1] "\" type mpegvideo alias mp3", NULL, 0, NULL) != 0) {
+        printf("Erro ao abrir o arquivo MP3\n");
+        return -1;
+    }
+
+    // Reproduz o arquivo MP3
+    if (mciSendStringA("play mp3", NULL, 0, NULL) != 0) {
+        printf("Erro ao reproduzir o arquivo MP3\n");
+        mciSendStringA("close mp3", NULL, 0, NULL); // Fecha o arquivo MP3
+        return -1;
+    }
+
+    printf("Pressione Enter para sair...\n");
+    getchar();
+
+    // Fecha o arquivo MP3
+    mciSendStringA("close mp3", NULL, 0, NULL);
+
+    return 0;
 }
