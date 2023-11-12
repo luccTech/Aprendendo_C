c6180ea993a31e5c057adff6207d0cf7d7c17999
Lucas Barbosa Barreto <lucasbarbosabarreto551@gmail.com>
2023-11-11 22:40:32 -0300

    dd

--- avaliações/joguinho_pH/pHtest.c
+++ avaliações/joguinho_pH/pHtest.c
@@ -1,60 +1,25 @@
-#include <windows.h>
-#include <mmsystem.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sndfile.h>
 
-// Função para reproduzir uma nota musical
-void playSound(float frequency, float duration) {
-    Beep(frequency, duration);
-}
-
-/*
-    | Nota | Frequência (Hz) |
-    |------|-----------------|
-    |  C6  | 1046,50         |
-    |  D6  | 1174,66         |
-    |  E6  | 1318,51         |
-    |  F6  | 1396,91         |
-    |  G6  | 1567,98         |
-    |  A6  | 1760,00         |
-    |  B6  | 1975,53         |
-    |  C7  | 2093,00         |
-    |  D7  | 2349,32         |
-    |  E7  | 2637,02         |
-    |  F7  | 2793,83         |
-    |  G7  | 3135,96         |
-    |  A7  | 3520,00         |
-    |  B7  | 3951,07         |
-    |  C8  | 4186,01         |
-*/
 int main() {
-    // Notas musicais e suas frequências
-    int notes[] = { 1046.50, 1174.66, 1318.51, 1396.91, 1567.98, 1760.00, 1975.53 };
+    const char *nome_arquivo = "caminho/do/arquivo.mp3";
 
-    // Duração de cada nota em milissegundos
-    int duration = 500;
+    SNDFILE *sndfile;
+    SF_INFO sfinfo;
 
-    float C6  = 1046.50;
-    float D6  = 1174.66;
-    float E6  = 1318.51;
-    float F6  = 1396.91;
-    float G6  = 1567.98;
-    float A6  = 1760.00;
-    float B6  = 1975.53;
-    float C7  = 2093.00;
-    float D7  = 2349.32;
-    float E7  = 2637.02;
-    float F7  = 2793.83;
-    float G7  = 3135.96;
-    float A7  = 3520.00;
-    float B7  = 3951.07;
-    float C8  = 4186.01;
+    sndfile = sf_open(nome_arquivo, SFM_READ, &sfinfo);
 
-    playSound(C6, 200);
-    Sleep(200);
-    playSound(C6, 200);
-    Sleep(200);
-    playSound(C6, 200);
-    Sleep(200);
-    playSound(C6, 200);
-    Sleep(200);
+    if (!sndfile) {
+        fprintf(stderr, "Erro ao abrir o arquivo: %s\n", sf_strerror(NULL));
+        return 1;
+    }
+
+    printf("Taxa de Amostragem: %d\n", sfinfo.samplerate);
+    printf("Número de Canais: %d\n", sfinfo.channels);
+    printf("Número de Quadros: %lld\n", sfinfo.frames);
+
+    sf_close(sndfile);
+
     return 0;
 }
--- avaliações/joguinho_pH/testSom.c
+++ avaliações/joguinho_pH/testSom.c
@@ -1,45 +1,25 @@
-#include <Python.h>
+#include <SDL2/SDL.h>
 
-int main() {
-    // Inicializa o interpretador Python
-    Py_Initialize();
-
-    // Importa o módulo Python
-    PyObject* pName = PyUnicode_DecodeFSDefault("funcao_python");
-    PyObject* pModule = PyImport_Import(pName);
-    Py_XDECREF(pName);
-
-    // Verifica se o módulo foi importado com sucesso
-    if (pModule != NULL) {
-        // Obtém a referência para a função Python
-        PyObject* pFunc = PyObject_GetAttrString(pModule, "minha_funcao");
-
-        // Verifica se a função foi encontrada
-        if (pFunc != NULL && PyCallable_Check(pFunc)) {
-            // Chama a função Python
-            PyObject* pValue = PyObject_CallObject(pFunc, NULL);
-
-            // Converte o resultado para uma string C
-            if (pValue != NULL) {
-                char* result = PyUnicode_AsUTF8(pValue);
-                printf("Resultado da função Python: %s\n", result);
-                Py_XDECREF(pValue);
-            } else {
-                PyErr_Print();
-            }
-            Py_XDECREF(pFunc);
-        } else {
-            if (PyErr_Occurred()) PyErr_Print();
-            printf("Erro: Não foi possível encontrar a função Python\n");
-        }
-        Py_XDECREF(pModule);
-    } else {
-        PyErr_Print();
-        printf("Erro: Não foi possível importar o módulo Python\n");
+int main(){
+    if (SDL_Init(SDL_INIT_AUDIO) < 0) {
+    printf("Erro ao inicializar o SDL: %s\n", SDL_GetError());
+    return 1;
     }
 
-    // Finaliza o interpretador Python
-    Py_Finalize();
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+    printf("Erro ao inicializar o sistema de áudio: %s\n", Mix_GetError());
+    return 1;
+    }
+    Mix_Music *musica = Mix_LoadMUS("avaliações/joguinho_pH/perdeu.mp3");
 
-    return 0;
+    if (!musica) {
+        printf("Erro ao carregar a música: %s\n", Mix_GetError());
+        return 1;
+    }
+
+    Mix_PlayMusic(musica, 1);
+    Mix_FreeMusic(musica);
+    Mix_CloseAudio();
+    SDL_Quit();
+
 }
